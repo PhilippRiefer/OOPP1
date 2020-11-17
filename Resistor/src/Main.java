07c1cc0e27cd679c2c86465f11c254d66757cc1e
PhilippRiefer <66263398+PhilippRiefer@users.noreply.github.com>
2020-11-17 14:17:32 +0100

    Add files via upload

---
+++ Resistor/src/Main.java
@@ -0,0 +1,39 @@
+
+public class Main {
+	
+	enum Prefix{
+		nano,
+		mikro,
+		milli,
+		none,
+		Kilo,
+		Mega,
+		Giga
+	}
+	
+	/*
+	//attributes
+	private int exponent; //for example milli should have exponent = -3
+	private String symbol; //for example milli should have "m"
+	
+	//constructor
+	public Prefix(int exponent, String symbol){
+		this.prefix = ; //?
+	}
+	
+	//methods
+	public int getExponent(){
+		this.exponent = exponent;
+	}
+	public String getSymbol(){
+		this.symbol = symbol;
+	}
+	public String toString(){
+		
+	} // "" in case of "none"
+	*/
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ Resistor/src/Resistor.java
@@ -0,0 +1,17 @@
+
+public class Resistor {
+	/*
+	//attributes
+	private double value;
+	private Prefix prefix;
+	
+	//constructors
+	public Resistor(double value);
+	public Resistor(Prefix prefix, double value);
+	
+	//methods
+	public double getValue();
+	public Prefix getPrefix();
+	*/
+
+}
